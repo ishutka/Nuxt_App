9c2509766a63521d449727908518b1c94570d67c
Hanna <ishutkinanna@gmail.com>
2021-04-10 22:47:31 +0300

    login page component was created

---
+++ pages/login.vue
@@ -0,0 +1,34 @@
+<template>
+  <form>
+    <h1>Login</h1>
+    <div class="form-group">
+      <input type="text" class="form-control" />
+    </div>
+    <div class="form-group">
+      <input type="text" class="form-control" />
+    </div>
+    <div class="bottom">
+      <button class="btn btn-primary">LogIn</button>
+      <nuxt-link to="/">Home</nuxt-link>
+    </div>
+  </form>
+</template>
+<script>
+export default {
+  layout: "empty"
+};
+</script>
+<style lang="scss" scoped>
+.bottom {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+button {
+  margin-right: auto;
+}
+form {
+  max-width: 600px;
+  min-width: 300px;
+}
+</style>
