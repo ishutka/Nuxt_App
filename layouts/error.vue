08b72d7f0b4f8edf656f47e063dbe534b9a5a5e4
Hanna <ishutkinanna@gmail.com>
2021-04-10 17:40:15 +0300

    error layout was created

---
+++ layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+        <h1>Error 404</h1>
+        <p>Page not found</p>
+        <nuxt-link to="/">Home</nuxt-link>
+    </section>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+h1 {
+    color: red;
+}
+section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+</style>
