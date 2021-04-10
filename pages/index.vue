ad9f7c0c875ac0778c51cd76e34c5529ed15c637
Hanna <ishutkinanna@gmail.com>
2021-04-10 16:30:35 +0300

    bootstrap connection & removing default fooling code

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,7 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        nuxt_app
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <section>
+    <h1>This is Home page!</h1>
+  </section>
 </template>
 
 <script>
