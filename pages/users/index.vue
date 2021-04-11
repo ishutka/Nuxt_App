df40795162a99fe8099decdd789ccb6d00e6b27b
Hanna <ishutkinanna@gmail.com>
2021-04-11 20:36:13 +0300

    asyncData was added to user Component

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,24 +1,31 @@
 <template>
   <section>
-    <h1>Users Page</h1>
+    <h1>{{ pageTitle }}</h1>
     <ol>
       <li v-for="user in users" :key="user">
-          <a href="#" @click.prevent="goTo(user)">User {{ user }}</a>
-          </li>
+        <a href="#" @click.prevent="goTo(user)">User {{ user }}</a>
+      </li>
     </ol>
   </section>
 </template>
 <script>
 export default {
+  asyncData() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve({ users: [1, 2, 3, 4, 5] });
+      },2000);
+    });
+  },
   data() {
     return {
-      users: [1, 2, 3, 4, 5]
+      pageTitle: "Users Page"
     };
   },
-methods:{
-    goTo(u){
-        this.$router.push('/users/'+ u);
+  methods: {
+    goTo(u) {
+      this.$router.push("/users/" + u);
     }
-}
+  }
 };
 </script>
