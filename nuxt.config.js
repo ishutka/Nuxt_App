472214e039526fe4343b5b5843c4d98da74c8438
Hanna <ishutkinanna@gmail.com>
2021-04-11 20:23:38 +0300

    color of loading bar was added to nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
-
+  loading: { color: "#9df4db" },
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
