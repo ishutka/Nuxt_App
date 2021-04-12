20104ed105dd653071380e1076d6126b463624c9
Hanna <ishutkinanna@gmail.com>
2021-04-12 09:40:42 +0300

    creation users store and set fetchUsers action

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -3,24 +3,23 @@
     <h1>{{ pageTitle }}</h1>
     <ol>
       <li v-for="user in users" :key="user.id">
-        <a href="#" @click.prevent="goTo(user)">{{ user.name }} ( {{user.email}} )</a>
-        
+        <a href="#" @click.prevent="goTo(user)"
+          >{{ user.name }} ( {{ user.email }} )</a
+        >
       </li>
     </ol>
   </section>
 </template>
 <script>
 export default {
-  asyncData({ $axios, error }) {
-    return $axios
-      .get("https://jsonplaceholder.typicode.com/users")
-      .then((params) => {
-        const users=params.data;
-        return { users };
-      })
-      .catch(err => {
-        error(err);
-      });
+  async asyncData({ store, error }) {
+    
+    try {
+      const users = await store.dispatch("users/fetchUsers");
+      return { users };
+    } catch (e) {
+      error(e);
+    }
   },
   data() {
     return {
